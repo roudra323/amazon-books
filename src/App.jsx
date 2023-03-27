d7b5ba6b787d079fee1025f09fba3bf47ecff46d
roudra323 <asirroudra@gmail.com>
2023-03-27 14:09:37 +0600

    last commit with updated css

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,7 @@ export default function BookList() {
   return (
     <section className="book-list">
       {data.map((book) => {
-        const { id, title, author, img } = book;
-        return <Book book={book} />;
+        return <Book key={book.id} book={book} />;
       })}
     </section>
   );
@@ -15,11 +14,10 @@ export default function BookList() {
 
 function Book(props) {
   const { id, title, author, img } = props.book;
-  console.log(id, title, author, img);
   return (
     <article className="book">
       <h4>{id}</h4>
-      <img src={`/images/${img}`} alt="" />
+      <img src={`/images/${img}`} alt="" className="book--cover" />
       <h1>{title}</h1>
       <h4>{author}</h4>
     </article>
